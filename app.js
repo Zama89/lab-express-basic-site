d3f46bd2e8c2a859c9d786df2c39e7cf85dd78c5
Jon Moliner <jonmolinerzama@gmail.com>
2021-11-22 21:10:05 +0100

    ultima prueba

---
+++ app.js
@@ -0,0 +1,22 @@
+const { request, response } = require("express");
+const express = require ("express");
+const app = express();
+const port = 2000;
+
+app.use(express.static('public'));
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/views/home.html");
+});
+
+app.get('/works', (request, response) => {
+    response.sendFile (__dirname + "/views/works.html");
+});
+
+app.get('/about-me', (request, response) => {
+    response.sendFile (__dirname +'/views/about-me.html');
+});
+
+app.listen(port, () => {
+    console.log(`Ejemplo en el lab en http://localhost:${post}`);
+});
